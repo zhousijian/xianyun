8877699e9cda3f390664c5975a39fd94f7bae3c3
zhousijian <13553593242@163.com>
2020-01-17 00:05:14 +0800

    完成头部和尾部的组件插入，和一些静态样式

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
+    <!-- 头部 -->
+    <Header></Header>
+    <!-- 轮播图 -->
     <nuxt />
+    <!-- 页脚 -->
+    <Footer></Footer>
   </div>
 </template>
 
-<style>
+<script>
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+export default {
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
+
+
+<style lang="less">
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -17,39 +34,22 @@ html {
   box-sizing: border-box;
 }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
+* {
   margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+ul,
+li,
+ol {
+  list-style: none;
+}
+a {
   text-decoration: none;
-  padding: 10px 30px;
+  color: inherit;
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+em,
+i {
+  font-style: normal;
 }
 </style>
